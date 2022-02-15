0a0a7a1061be91d43c10ce530e0f41c1bc083ee5
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-02-15 15:39:22 +0000

    Added README and header files

---
+++ sort.h
@@ -0,0 +1,31 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define INCREMENT 1
+#define DECREMENT -1
+
+/**
+ * struct listint_s - Doubly linked list node
+ * @n: integer
+ * @prev: pointer to previous element
+ * @next: pointer to next element
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *, size_t);
+void print_list(const listint_t *);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
