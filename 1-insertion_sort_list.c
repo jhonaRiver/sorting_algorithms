04df7fa4b5f47e6692c768b8ba64099348ccf245
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-02-15 21:58:02 +0000

    Added files task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in
+ * ascending order using Insertion sort algorithm
+ * @list: Doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	bool flag = false;
+	listint_t *tmp = NULL, *aux = NULL;
+
+	if (!list || !(*list) || !(*list)->next)
+		return;
+	tmp = *list;
+	while (tmp->next)
+	{
+		if (tmp->n > tmp->next->n)
+		{
+			tmp->next->prev = tmp->prev;
+			if (tmp->next->prev)
+				tmp->prev->next = tmp->next;
+			else
+				*list = tmp->next;
+			tmp->prev = tmp->next;
+			tmp->next = tmp->next->next;
+			tmp->prev->next = tmp;
+			if (tmp->next)
+				tmp->next->prev = tmp;
+			tmp = tmp->prev;
+			print_list(*list);
+			if (tmp->prev && tmp->prev->n > tmp->n)
+			{
+				if (!flag)
+					aux = tmp->next;
+				flag = true;
+				tmp = tmp->prev;
+				continue;
+			}
+		}
+		if (!flag)
+			tmp = tmp->next;
+		else
+			tmp = aux, flag = false;
+	}
+}
